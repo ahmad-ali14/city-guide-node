6dafd13ce836753ed316b86800301e6391458abe
Ahmad Ali <aallii300300@gmail.com>
2020-06-25 22:48:57 +0100

    prepare the server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var cors = require('cors')
+
 
 var app = express();
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json({
@@ -8,7 +12,13 @@ app.get('/', (req, res) => {
     })
 })
 
-const Port = 3000;
+app.get('/pharmacies', (req, res) => {
+    res.json({
+        Author: 'ahmad'
+    })
+})
+
+const Port = 5000;
 
 app.listen(Port, () => {
     console.log(`app listening on port ${Port}`);
