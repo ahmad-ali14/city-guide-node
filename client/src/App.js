31a04563236f804e4e2d466a9e32f9aaf10cbc76
Ahmad Ali <aallii300300@gmail.com>
2020-06-29 20:45:49 +0100

    error handlling

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import axios from 'axios';
 import ReactLoading from 'react-loading';
 
+const server_url = "http://localhost:5000";
 
 function App() {
   const [isVisible, setIsVisible] = useState({
-    pharmacies: true,
+    pharmacies: false,
     highSchools: false,
     colleges: false,
     hospitals: false,
@@ -16,34 +17,46 @@ function App() {
 
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [city, setCity] = useState('');
+  const [err, setErr] = useState(false);
 
-  useEffect(() => { grapPharmacies(); }, [])
+  // useEffect(() => { grapPharmacies(); }, [])
 
 
   const grapPharmacies = async () => {
+    if (city === '') { setErr(true); return; }
+    setErr(false);
     await setLoading(true);
-    axios.get('http://localhost:5000/pharmacies').then((res) => {
+    axios.get(server_url + `/${city}/pharmacies`, {
+      city: city
+    }).then((res) => {
       setData(res.data)
     }).then(() => setLoading(false))
   }
 
   const grapColleges = async () => {
+    if (city === '') { setErr(true); return; }
+    setErr(false);
     await setLoading(true);
-    axios.get('http://localhost:5000/colleges').then((res) => {
+    axios.get(server_url + `/${city}/colleges`).then((res) => {
       setData(res.data)
     }).then(() => setLoading(false))
   }
 
   const grapHospitals = async () => {
+    if (city === '') { setErr(true); return; }
+    setErr(false);
     await setLoading(true);
-    axios.get('http://localhost:5000/hospitals').then((res) => {
+    axios.get(server_url + `/${city}/hospitals`).then((res) => {
       setData(res.data)
     }).then(() => setLoading(false))
   }
 
   const grapDoctors = async () => {
+    if (city === '') { setErr(true); return; }
+    setErr(false);
     await setLoading(true);
-    axios.get('http://localhost:5000/doctors').then((res) => {
+    axios.get(server_url + `/${city}/doctors`).then((res) => {
       setData(res.data)
     }).then(() => setLoading(false))
   }
@@ -114,7 +127,21 @@ function App() {
   }
   return (
     <div className="container">
-      <h1 className="text-center pt-5 pb-5" >Harrow Mini Guide</h1>
+      <h1 className="text-center pt-5 pb-5" >City Mini Guide</h1>
+
+      <hr />
+      <span>choose a city:</span>{' '}{' '} {' '}{' '} {' '}{' '}
+      <select style={{ display: 'inline' }} id="citySelector" autoFocus="-1" value={city} onChange={(e) => { console.log('e value', e.target.value); setCity(e.target.value); }}>
+        <option id="defaultcitySelection" value="no-city" defaultValue='' >select a city </option>
+        <option value='Harrow'>Harrow</option>
+        <option value="stratford%2C+east+london" >Stratford</option>
+        <option value='heathrow'>Heathrow</option>
+      </select>
+
+      {
+        err && <><hr className="mt-2 mb-2" /><div className="alert-danger" ><h5>check out your request, a city and category must be chosen, No default values are provided</h5></div></>
+      }
+      <hr />
       <ul className="nav nav-pills nav-fill">
         <li className="nav-item">
           <button className={isVisible.pharmacies ? "nav-link active" : "nav-link"} onClick={(e) => { grapPharmacies(); handleTabClick(e) }} value="pharmacies" >Pharmacies</button>
@@ -133,7 +160,17 @@ function App() {
         </li>
       </ul>
 
-      <hr className="mb-5" />
+
+
+      {/* <hr />
+      <div>
+        <span>show: {' '} {' '} {' '} {' '}</span>
+        <div style={{ display: 'inline' }} class="custom-control custom-checkbox">
+          <input type="checkbox" class="custom-control-input" id="defaultChecked2" checked />
+          <label class="custom-control-label" for="defaultChecked2">Default checked</label>
+        </div>
+      </div> */}
+      <br className="mb-5" />
 
       {loading ? (
         <div className="text-center" style={{ marginLeft: '40%' }} >
@@ -152,7 +189,7 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {data && data.length > 0 && data.map((place, i) => {
+              {(data && data.length > 0) ? data.map((place, i) => {
                 return (
                   <tr key={i}>
                     <th scope="row">{i + 1}</th>
@@ -162,7 +199,10 @@ function App() {
                     <td>{place.website ? <a href={place.website} target="_blank" >{place.name}</a> : 'N/A'}</td>
                   </tr>
                 );
-              })}
+              }) : (
+                  <tr><td colSpan='5' ><h3 className="text-center"> choose a city and category first </h3></td></tr>
+                )
+              }
 
             </tbody>
           </table>
